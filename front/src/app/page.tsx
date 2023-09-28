e04ef8d85fc4f97f714f88283cf788f1b3034a5e
Angel Calderon <angelcalderon303@gmail.com>
2023-09-28 11:31:44 -0400

    cree un bug xd

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 lg:max-w-[60rem] lg:mb-0 ">
+      <div className="mb-32 sm:max-w-[60rem] lg:mb-0 ">
         <Intro/>
       </div>
     </main>
